f7abe90750a73611f7e638194befaf33ff9c97f1
dawnnnnn <tan32211@gmail.com>
2016-09-01 18:43:40 +0800

    update DCTagButton

--- DCTagView/TagView/DCTagButton.h
+++ DCTagView/TagView/DCTagButton.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "DCTag.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface DCTagButton : UIButton
 
+@property (nonatomic, strong) DCTag *mtag;
+
++ (instancetype)buttonWithTag: (DCTag *)tag;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- DCTagView/TagView/DCTagButton.m
+++ DCTagView/TagView/DCTagButton.m
@@ -10,12 +10,71 @@
 
 @implementation DCTagButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
++ (instancetype)buttonWithTag: (DCTag *)tag {
+    DCTagButton *btn = [super buttonWithType:UIButtonTypeCustom];
+    
+    btn.mtag = tag;
+    
+    if (tag.attributedText) {
+        [btn setAttributedTitle:tag.attributedText forState:UIControlStateNormal];
+    } else {
+        [btn setTitle:tag.text forState:UIControlStateNormal];
+        [btn setTitleColor:tag.textColor forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+        btn.titleLabel.font = tag.font ?: [UIFont systemFontOfSize:tag.fontSize];
+    }
+    
+    btn.backgroundColor = tag.bgColor;
+    btn.contentEdgeInsets = tag.padding;
+    btn.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+    
+    if (tag.bgImg) {
+        [btn setBackgroundImage:tag.bgImg forState:UIControlStateNormal];
+    }
+    
+    if (tag.borderColor) {
+        btn.layer.borderColor = tag.borderColor.CGColor;
+    }
+    
+    if (tag.borderWidth) {
+        btn.layer.borderWidth = tag.borderWidth;
+    }
+    
+    btn.userInteractionEnabled = tag.enable;
+    if (tag.enable) {
+        UIColor *highlightedBgColor = tag.highlightedBgColor ?: [self darkerColor:btn.backgroundColor];
+        [btn setBackgroundImage:[self imageWithColor:highlightedBgColor] forState:UIControlStateHighlighted];
+    }
+    
+    btn.layer.cornerRadius = tag.cornerRadius;
+    btn.layer.masksToBounds = YES;
+    
+    return btn;
 }
-*/
+
+
++ (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
++ (UIColor *)darkerColor:(UIColor *)color {
+    CGFloat h, s, b, a;
+    if ([color getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:b * 0.85
+                               alpha:a];
+    return color;
+}
 
 @end
