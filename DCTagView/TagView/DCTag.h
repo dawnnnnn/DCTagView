6681329456effd5570cf5071b485e5f3033e023e
dawnnnnn <tan32211@gmail.com>
2016-09-01 18:38:25 +0800

    update DCTag model

--- DCTagView/TagView/DCTag.h
+++ DCTagView/TagView/DCTag.h
@@ -7,7 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface DCTag : NSObject
 
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, copy) NSAttributedString *attributedText;
+
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong) UIColor *bgColor;
+@property (nonatomic, strong) UIColor *highlightedBgColor;
+@property (nonatomic, strong) UIColor *borderColor;
+
+@property (nonatomic, strong) UIImage *bgImg;
+@property (nonatomic, strong) UIFont *font;
+
+@property (nonatomic, assign) CGFloat cornerRadius;
+@property (nonatomic, assign) CGFloat borderWidth;
+///like padding in css
+@property (nonatomic, assign) UIEdgeInsets padding;
+///if no font is specified, system font with fontSize is used
+@property (nonatomic, assign) CGFloat fontSize;
+///default:YES
+@property (nonatomic, assign) BOOL enable;
+
+- (nonnull instancetype)initWithText: (nonnull NSString *)text;
++ (nonnull instancetype)tagWithText: (nonnull NSString *)text;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- DCTagView/TagView/DCTag.m
+++ DCTagView/TagView/DCTag.m
@@ -8,6 +8,31 @@
 
 #import "DCTag.h"
 
+static CGFloat const kDefaultFontSize = 14.0;
+
 @implementation DCTag
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _fontSize = kDefaultFontSize;
+        _textColor = [UIColor blackColor];
+        _bgColor = [UIColor whiteColor];
+        _enable = YES;
+    }
+    return self;
+}
+
+- (instancetype)initWithText: (NSString *)text {
+    self = [self init];
+    if (self) {
+        _text = text;
+    }
+    return self;
+}
+
++ (instancetype)tagWithText: (NSString *)text {
+    return [[self alloc] initWithText: text];
+}
 
 @end
